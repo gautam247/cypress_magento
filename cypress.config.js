d268ded75e05cfb884cac470dfbce29fdc35f543
gautam247 <133091543+gautam247@users.noreply.github.com>
2024-08-18 14:37:29 +0530

    cypress_demo

---
+++ cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild")
+
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  )
+  return config
+};
+module.exports = defineConfig({
+  e2e: {
+    viewportHeight:1200,
+    viewportWidth:1400,
+    defaultCommandTimeout: 60000,
+    pageLoadTimeout: 60000,
+    responseTimeout:30000,
+    requestTimeout: 30000,
+    setupNodeEvents,
+    specPattern: ["cypress/e2e/features/*.feature"],
+    chromeWebSecurity: false
+  },
+});
